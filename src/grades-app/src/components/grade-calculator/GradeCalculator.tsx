b639e58e169dc54e1622fecc6e420539b97d4c40
angusho1 <57027339+angusho1@users.noreply.github.com>
2022-11-05 13:04:53 -0700

    Add input change handlers

--- src/grades-app/src/components/grade-calculator/GradeCalculator.tsx
+++ src/grades-app/src/components/grade-calculator/GradeCalculator.tsx
@@ -2,15 +2,31 @@ import { Button, Card, CardActions, CardContent, Grid, Stack, TextField, Typogra
 import React, { useState } from 'react'
 import { Assignment } from '../../types/Assignment';
 
+const round = (num: number) => Math.round(num * 100) / 100;
+
 export default function GradeCalculator() {
   const defaultAssignments: Assignment[] = [
-    { id: '1', name: 'Assignment 1', pct: 0.7, weight: 0.3, hasGrade: true },
-    { id: '2', name: 'Assignment 2', pct: 0.6, weight: 0.2, hasGrade: true },
+    { id: '1', name: 'Assignment 1', pct: 0.72, weight: 0.3, hasGrade: true },
+    { id: '2', name: 'Assignment 2', pct: 0.64, weight: 0.2, hasGrade: true },
     { id: '3', name: 'Assignment 3', pct: null, weight: 0.2, hasGrade: false },
     { id: '4', name: 'Assignment 4', pct: null, weight: 0.3, hasGrade: false },
   ];
   const [desiredGrade, setDesiredGrade] = useState<number>(1);
   const [assignments, setAssignments] = useState<Assignment[]>(defaultAssignments);
+
+  const onDesiredGradeChange = (e: any) => {
+    const value = e.target.value;
+    if (!isNaN(value)) {
+      const val = round(value / 100);
+      setDesiredGrade(val);
+      console.log(val);
+    }
+  };
+
+  const changeAssignmentGrade = (id: string, value: number) => {
+    console.log(id);
+    console.log(value);
+  };
 
   return (
     <Grid container sx={{ p: 10 }}>
@@ -29,12 +45,13 @@ export default function GradeCalculator() {
                   InputLabelProps={{
                     shrink: true,
                   }}
-                  defaultValue={desiredGrade}
+                  defaultValue={desiredGrade * 100}
                   inputProps={{
                     min: 0,
-                    max: 1,
-                    step: 0.1
+                    max: 100,
+                    step: 1
                   }}
+                  onChange={onDesiredGradeChange}
                 />%
               </Grid>
             </Grid>
@@ -44,7 +61,7 @@ export default function GradeCalculator() {
             <Stack sx={{ mt: 4 }} spacing={3}>
               { assignments.length > 0 && (
                   assignments.map(assignment => (
-                    <Grid container>
+                    <Grid container key={assignment.id}>
                       <Grid item xs={9} sx={{ alignItems: 'left' }}>
                         <Typography>{ assignment.name }</Typography>
                       </Grid>
@@ -55,13 +72,20 @@ export default function GradeCalculator() {
                           InputLabelProps={{
                             shrink: true,
                           }}
-                          defaultValue={assignment.pct}
+                          onChange={(e: any) => {
+                            const value = e.target.value;
+                            if (!isNaN(value)) {
+                              const val = round(value / 100);
+                              changeAssignmentGrade(assignment.id, val);
+                            }
+                          }}
+                          defaultValue={assignment.pct ? assignment.pct * 100 : 0}
                           inputProps={{
                             min: 0,
-                            max: 1,
-                            step: 0.1
+                            max: 100,
+                            step: 1,
                           }}
-                        />%
+                          />%
                       </Grid>
                     </Grid>
                   ))
